9f9d674401c902fde0827fc55fec2c28e3895a22
Quentin Saillard <quentin.saillard01@gmail.com>
2024-05-29 17:28:41 +0200

    Update ActionServlet.java

--- WebAppp/src/main/java/fr/insalyon/dasi/test/webappp/controllers/ActionServlet.java
+++ WebAppp/src/main/java/fr/insalyon/dasi/test/webappp/controllers/ActionServlet.java
@@ -1,15 +1,17 @@
+package fr.insalyon.dasi.test.webappp.controllers;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fr.insalyon.dasi.test.webappp.models;
 
-import business.model.Presenter;
-import business.model.Student;
-import fr.insalyon.dasi.test.webappp.controllers.AuthentifierUtilisateurAction;
+import dao.JpaUtil;
+import fr.insalyon.dasi.test.webappp.vue.ProfilUtilisateurSerialisation;
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,31 +19,53 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author qsaillard
  */
-public abstract class Action {
-    public abstract Object execute(HttpServletRequest request, String login, String password);
-    
-    public void authentification(HttpServletRequest request, HttpServletResponse response)
+@WebServlet(urlPatterns = {"/ActionServlet"})
+public class ActionServlet extends HttpServlet {
+
+     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        AuthentifierUtilisateurAction authService = new AuthentifierUtilisateurAction();
-        
-        String login = request.getParameter("login");
-        String password = request.getParameter("password");
-        
-        Object user = authService.execute(request, login, password);
-        if (user != null) {
-            if (user instanceof Student) {
-                // Logique spécifique pour les employés
-                request.setAttribute("user", (Student) user);
-            } else if (user instanceof Presenter) {
-                // Logique spécifique pour les intervenants
-                request.setAttribute("user", (Presenter) user);
-            }
+        response.setContentType("application/json;charset=UTF-8");
+
+        String todo = request.getParameter("todo");
+        if ("connecter".equals(todo)) {
+            System.out.println("[TEST] Appel de l’ActionServlet");
+
+            AuthentifierUtilisateurAction action = new AuthentifierUtilisateurAction();
+            action.authentification(request, response);
+
+            ProfilUtilisateurSerialisation serialization = new ProfilUtilisateurSerialisation();
+            serialization.serialize(request, response);
         } else {
-            // Gérer l'authentification échouée
-            request.setAttribute("error", "Invalid login or password");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid 'todo' parameter.");
         }
     }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Main servlet controller";
+    }
     
-    
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        JpaUtil.creerFabriquePersistance();
+    }
+
+    @Override
+    public void destroy() {
+        JpaUtil.fermerFabriquePersistance();
+        super.destroy();
+    }
 }
