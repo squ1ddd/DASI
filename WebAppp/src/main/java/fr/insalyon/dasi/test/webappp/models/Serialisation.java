c2d964e9d191f1f6085b03ce9611cfb7485192cb
Quentin Saillard <quentin.saillard01@gmail.com>
2024-05-29 17:29:58 +0200

    Update Serialisation.java

--- WebAppp/src/main/java/fr/insalyon/dasi/test/webappp/models/Serialisation.java
+++ WebAppp/src/main/java/fr/insalyon/dasi/test/webappp/models/Serialisation.java
@@ -5,13 +5,40 @@
  */
 package fr.insalyon.dasi.test.webappp.models;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 /**
  *
  * @author qsaillard
  */
 public abstract class Serialisation {
-    public abstract void serialize(HttpServletRequest request, HttpServletResponse response) throws IOException;
+    
+    private Gson gson;
+
+    public Serialisation() {
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    public void serialize(Object object, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        if (object instanceof TestUtilisateur) {
+            serializeTestUtilisateur((TestUtilisateur) object, response);
+        } else {
+            serializeUnknown(object, response);
+        }
+    }
+
+    private void serializeTestUtilisateur(TestUtilisateur utilisateur, HttpServletResponse response) throws IOException {
+        String json = gson.toJson(utilisateur);
+        response.getWriter().write(json);
+    }
+
+    private void serializeUnknown(Object object, HttpServletResponse response) throws IOException {
+        String json = gson.toJson(object);
+        response.getWriter().write(json);
+    }
 }
